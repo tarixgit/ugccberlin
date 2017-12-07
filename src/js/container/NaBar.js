86f1c9231cc0c05ede3834d862b016bcdaf0ad65
tarix <tarix2006@gmail.com>
2017-12-07 04:33:01 +0100

    -- fixes

--- src/js/container/NaBar.js
+++ src/js/container/NaBar.js
@@ -9,35 +9,28 @@ class NavBar extends React.Component {
     render() {
         return (
             <div role="navigation">
-                { /*<Link to="/">Про УГКЦ Берлін</Link>
-                <Link to="/calendar">Календар</Link>
-                <span > | </span>
-                <Link to="/info">Ввійти</Link>
-                <span>or</span>
-                <Link to="/news">news</Link>
-                */}
                 <Navbar inverse collapseOnSelect>
                     <Navbar.Header>
                         <Navbar.Brand>
-                            <Link to="/">React-Bootstrap</Link >
+                            <Link to="/">УГКЦ Берлін</Link >
                         </Navbar.Brand>
                         <Navbar.Toggle />
                     </Navbar.Header>
                     <Navbar.Collapse>
                         <Nav>
-                            <NavItem eventKey={1} href="/calendar"><Link  to="/info">Link</Link></NavItem>
-                            <NavItem eventKey={2} href="/calendar"><Link to="/info">Link</Link></NavItem>
-                            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                            <NavItem eventKey={1} href="/calendar"><Link  to="/info">Новини</Link></NavItem>
+                            <NavItem eventKey={2} href="/calendar"><Link to="/info">Календар</Link></NavItem>
+                            {/*<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
                                 <MenuItem eventKey={3.1} href="/info"><Link  to="/info">Action</Link></MenuItem>
                                 <MenuItem eventKey={3.2} href="/news"><Link  to="/news">Another action</Link></MenuItem>
                                 <MenuItem eventKey={3.3} href="/info"><Link  to="/info">Something else here</Link></MenuItem>
                                 <MenuItem divider />
                                 <MenuItem eventKey={3.3} href="/news"><Link  to="/news">Separated link</Link></MenuItem>
-                            </NavDropdown>
+                            </NavDropdown>*/}
                         </Nav>
                         <Nav pullRight>
-                            <NavItem eventKey={1} href="#">Link Right</NavItem>
-                            <NavItem eventKey={2} href="#">Link Right</NavItem>
+                            <NavItem eventKey={1} href="#">Інфо</NavItem>
+                            <NavItem eventKey={2} href="#">Контакти</NavItem>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
